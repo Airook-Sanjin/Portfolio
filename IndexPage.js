6dd0d15dbc9150a17838e83176b0a665026abee3
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-02-28 21:31:16 -0500

    Added Welcome-Screen and added some descriptions

---
+++ IndexPage.js
@@ -0,0 +1,20 @@
+// Transition for the loading screen
+let WelcomeScreen = document.getElementById("WelcomeScreen");
+WelcomeScreen.style.animation = "opacity";
+let FirstTime = false;
+//If FirstTime is not in session storage it will run animation
+if (!sessionStorage.getItem("FirstTime?")) {
+  setTimeout(function () {
+    WelcomeScreen.style.opacity = "0"; //Fade out
+    setTimeout(() => {
+      WelcomeScreen.style.display = "none";
+      FirstTime = true;
+      sessionStorage.setItem("FirstTime?", FirstTime);
+    }, 1000);
+  }, 3000);
+// loading screen will not appear
+} else {
+  WelcomeScreen.style.display = "none";
+}
+
+//-----------------------------------------
